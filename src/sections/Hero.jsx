31bbcf0860041841fcb8dc3629eb0df2b3632933
natan-teklie <116352605+natan-teklie@users.noreply.github.com>
2025-07-19 12:41:40 +0300

    infrastructure

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='hero' className='relative overflow-hidden'>
+      <div className="absolute top-0 left-0 z-10"><img src="" alt="" />Hello Nati please be decipline and fast enough</div>
+      <div className="hero-layout">
+        <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5 '>
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              
+              <h1>shaping Design</h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+              <h1>Hello Moti you will be the future software devloper</h1>
+
+
+            </div>
+          </div>
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
