58b26ab8073449835f427c38a371cd1bfb95f1e9
natan-teklie <116352605+natan-teklie@users.noreply.github.com>
2025-08-16 12:30:16 +0300

    the navBar completed at high level

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { navLinks } from "../Constants";
 
 const NavBar = () => {
+  const [scrolled, setScrolled] = useState(false)
+  useEffect(()=>{
+    const handleScroll = ()=>{
+      const isscrolled = window.scrollY > 10;
+      setScrolled(true)
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
   return (
-    <header className='navbar'>
-<div className="inner">
-    <a href="#hero" className="logo">Natan Teklie | FSWD</a>
-    <nav className="desktop"></nav>
-    <a href="#contact" className="contact-btn group">
+    <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'} `}>
       <div className="inner">
-        <span>Contact me</span>
+        <a href="#hero" className="logo">
+          Natan Teklie | FSWD
+        </a>
+        <nav className="desktop">
+          <ul>
+          {navLinks.map(({ link, name }) => (
+            <li key={name} className="group">
+              <a href={link}>
+                <span> {name} </span>
+                <span className="underline" />
+              </a>
+            </li>
+          ))}
+          </ul>
+        </nav>
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact me</span>
+          </div>
+        </a>
       </div>
-    </a>
-</div>
     </header>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
